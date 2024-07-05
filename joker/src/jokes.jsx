6d6f6783df207069b751e5369012596e7a05d765
KUNAL CHOURE <chourekunal4@gmail.com>
2024-07-05 20:45:36 +0530

    added

--- joker/src/jokes.jsx
+++ joker/src/jokes.jsx
@@ -1,11 +1,15 @@
 import './jokes.css';
 export default function Jokes(){
+  const url="https://official-joke-api.appspot.com/random_joke";
     return(
         <>
         <div className="heading">
           <h1>JOKER</h1>
           <p>Finding smile ,its just a click away</p>
           </div>
+          <hr />
+
+          <p></p>
           <button>Find a Joke!</button>
         </>
     )
