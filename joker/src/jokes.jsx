4e3ca04e3b684c27a2b67e7e3945e28e8ed11b8c
KUNAL CHOURE <chourekunal4@gmail.com>
2024-07-05 19:52:48 +0530

    updated

--- joker/src/jokes.jsx
+++ joker/src/jokes.jsx
@@ -1,9 +1,12 @@
-export default function jokes(){
+import './jokes.css';
+export default function Jokes(){
     return(
         <>
+        <div className="heading">
           <h1>JOKER</h1>
           <p>Finding smile ,its just a click away</p>
-          
+          </div>
+          <button>Find a Joke!</button>
         </>
     )
 }
